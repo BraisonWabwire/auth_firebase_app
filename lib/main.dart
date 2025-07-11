47f9b99d014b8dd3033ab118201fd6894d30605e
Braison Wabwire <braisonwabwire2003@gmail.com>
2025-07-11 23:46:59 +0300

    Testing the UI

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Welcome",
+      home: Scaffold(
+        appBar: AppBar(title: Text("data"),),
+      ),
+    );
   }
 }
